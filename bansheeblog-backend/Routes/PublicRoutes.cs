3472819770be1982667369f8125fadec6db5f717
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-03-10 04:12:30 +0100

    Changes from anonymous to named render param class

--- bansheeblog-backend/Routes/PublicRoutes.cs
+++ bansheeblog-backend/Routes/PublicRoutes.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using BansheeBlog.Models;
@@ -34,12 +35,7 @@ namespace BansheeBlog.Routes
                     .Take(5)
                     .ToListAsync();
 
-                await res.RenderTemplate(templatePath, new
-                {
-                    Now = DateTime.UtcNow,
-                    Articles = articles,
-                    Settings = settings
-                });
+                await res.RenderTemplate(templatePath, new FrontpageRenderParam(DateTime.UtcNow, articles, settings));
             };
         }
 
@@ -48,6 +44,7 @@ namespace BansheeBlog.Routes
             return async (req, res) => await res.SendFile(Path.Combine(config.PublicDirectory, "admin", "index.html"));
         }
 
+
         public static Func<Request, Response, Task> FindFromSlug(Settings settings, Tracking tracking, SQLiteAsyncConnection db, Configuration config)
         {
             return async (req, res) =>
@@ -55,34 +52,65 @@ namespace BansheeBlog.Routes
                 var slug = req.Parameters["slug"];
                 if (settings.UseServerSideTracking)
                 {
-                    tracking.CollectInformation(req, "article" + slug);
+                    tracking.CollectInformation(req, "article/" + slug);
                 }
                 var article = await db.FindAsync<Article>(a => a.Public && a.Slug == slug);
                 if (article == null)
                 {
                     var templatePath = Path.Combine(config.ThemeDirectory, settings.ActiveTheme, "error.hbs");
-                    await res.RenderTemplate(templatePath, new
-                    {
-                        Now = DateTime.UtcNow,
-                        Settings = settings,
-                        title = "Article not found",
-                        message = $"The article with the slug '{slug}' was not found"
-                    });
+                    await res.RenderTemplate(templatePath, new ArticleNotFoundRenderParam(DateTime.UtcNow, settings, "Article not found", $"The article with the slug '{slug}' was not found"));
                 }
                 else
                 {
                     var htmlContent = await db.GetAsync<ArticleHtml>(article.Id);
                     article.Html = htmlContent.Content;
                     var templatePath = Path.Combine(config.ThemeDirectory, settings.ActiveTheme, "article.hbs");
-                    await res.RenderTemplate(templatePath, new
-                    {
-                        Now = DateTime.UtcNow,
-                        Article = article,
-                        Settings = settings
-                    });
+                    await res.RenderTemplate(templatePath, new ArticleRenderParam(DateTime.UtcNow, article, settings));
                 }
             };
         }
+    }
+    public class FrontpageRenderParam
+    {
+        public DateTime Now { get; }
+        public List<Article> Articles { get; }
+        public Settings Settings { get; }
 
+        public FrontpageRenderParam(DateTime now, List<Article> articles, Settings settings)
+        {
+            Now = now;
+            Articles = articles;
+            Settings = settings;
+        }
+    }
+
+    public class ArticleRenderParam
+    {
+        public DateTime Now { get; }
+        public Article Article { get; }
+        public Settings Settings { get; }
+
+        public ArticleRenderParam(DateTime now, Article article, Settings settings)
+        {
+            Now = now;
+            Article = article;
+            Settings = settings;
+        }
+    }
+    
+    public class ArticleNotFoundRenderParam
+    {
+        public DateTime Now { get; }
+        public Settings Settings { get; }
+        public string Title { get; }
+        public string Message { get; }
+
+        public ArticleNotFoundRenderParam(DateTime now, Settings settings, string title, string message)
+        {
+            Now = now;
+            Settings = settings;
+            Title = title;
+            Message = message;
+        }
     }
 }
