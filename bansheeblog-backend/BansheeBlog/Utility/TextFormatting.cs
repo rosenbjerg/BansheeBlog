63115f622d8a7a5f744fbe5616cef0ec3e0a74f0
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-04 02:14:55 +0200

    Set incoming dates to UTC

--- bansheeblog-backend/BansheeBlog/Utility/TextFormatting.cs
+++ bansheeblog-backend/BansheeBlog/Utility/TextFormatting.cs
@@ -1,32 +1,27 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
 using System.Net;
+using System.Runtime.Serialization;
 using System.Text.RegularExpressions;
 using BansheeBlog.Models;
+using Microsoft.AspNetCore.Http;
+using Red;
 
 namespace BansheeBlog.Utility
 {
     public static class TextFormatting
     {
-        private static readonly Regex FirstParagraphRegex = new Regex("(<p>[\\s\\S]*?<\\/p>)", RegexOptions.Compiled);
-
         public static string FirstParagraph(string content)
         {
-            var firstParagraph = FirstParagraphRegex.Match(content);
-            return content.Substring(0, firstParagraph.Index + firstParagraph.Length);
+            return content.Substring(0, content.IndexOf("</p>") + 4);
         }
 
-        public static string CreateUrlTitle(string title)
-        {
-            var urlTitle = title
-                .Replace(" ", "-")
-                .Replace("'", "")
-                .ToLowerInvariant()
-                .Trim();
-            return WebUtility.UrlEncode(urlTitle);
-        }
         public static void CopyMeta(Article existing, Article updated)
         {
-            existing.Edited = DateTime.UtcNow;
+            existing.Edited = DateTime.SpecifyKind(updated.Edited, DateTimeKind.Utc);
+            existing.Created = DateTime.SpecifyKind(updated.Created, DateTimeKind.Utc);
             existing.Title = updated.Title;
             existing.Slug = updated.Slug;
             existing.Tags = updated.Tags;
